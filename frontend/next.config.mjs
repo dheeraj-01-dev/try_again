1fc6ab0639085f0eacb41744aeb7093bb9800892
dheera-01-dev <dheeraj.01.dev@gmail.com>
2024-08-10 16:45:54 +0530

    ip setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    API_DOMAIN: "http://192.168.173.131:5000"
+    // API_DOMAIN: "http://192.168.173.131:5000"
+    API_DOMAIN: "http://157.10.99.99:5000"
   }
 };
 
